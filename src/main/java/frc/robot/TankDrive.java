b9bb8992abbfc2d3080b272849e1009d3897550c
CoPokBl <copokbl@serble.net>
2023-02-08 19:20:08 +1100

    Refactor and seperate code into classes

---
+++ src/main/java/frc/robot/TankDrive.java
@@ -0,0 +1,33 @@
+package frc.robot;
+
+import edu.wpi.first.wpilibj.motorcontrol.MotorController;
+
+public class TankDrive {
+
+    private MotorController[] LeftMotors;
+    public MotorController[] RightMotors;
+
+    public void Init(Hardware hardware) {
+        LeftMotors = new MotorController[] {
+            hardware.LeftMotor1,
+            hardware.LeftMotor2
+        };
+        RightMotors = new MotorController[] {
+            hardware.RightMotor1,
+            hardware.RightMotor2
+        };
+    }
+
+    public void SetLeft(double s) {
+        for (MotorController c : LeftMotors) {
+            c.set(s);
+        }
+    }
+
+    public void SetRight(double s) {
+        for (MotorController c : RightMotors) {
+            c.set(s);
+        }
+    }
+    
+}
