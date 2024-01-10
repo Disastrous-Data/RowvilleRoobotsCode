b1af39a2d0e8da8c8a670c54977414e7462be13b
copokbl <copokbl@serble.net>
2024-01-10 13:32:26 +1100

    document Main.java

--- src/main/java/com/disastrousdata/Main.java
+++ src/main/java/com/disastrousdata/Main.java
@@ -1,12 +1,15 @@
 // Copyright (c) Disastrous Data
-// Contributors (In order of contribution): CoPokBl, Camo
+// Contributors (In order of contribution): CoPokBl
+// Before making changes please read README.md in the project root.
 
 package com.disastrousdata;
 
 import edu.wpi.first.wpilibj.RobotBase;
 
+// This class should never be changed because it just starts the robot.
+// All actual robot code is in Robot.java
 public final class Main {
-  private Main() {}
+  private Main() { }
   public static void main(String... args) {
     RobotBase.startRobot(Robot::new);
   }
